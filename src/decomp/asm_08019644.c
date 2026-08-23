84e50aa81fcb053e652cdcfaf7f3c3cb50835d8d
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-08-23 11:59:26 -0400

    refactor: remove more register pins

--- src/decomp/asm_08019644.c
+++ src/decomp/asm_08019644.c
@@ -1,7 +1,9 @@
 #include "src/lib_sprite.h"
 
 void func_08019644(u32 arg0) {
-    register u32 value asm("r2") = arg0;
+    u32 value;
+
+    value = arg0;
     sprite_set_visible(gSpriteHandler,
                        *(s16 *)((u8 *)gCurrentSceneVariable + 0x68), (u16)value);
 }
--- src/decomp/asm_080f1574.c
+++ src/decomp/asm_080f1574.c
@@ -3,8 +3,11 @@
 extern u8 *D_030068E8;
 
 u32 func_080F1574(u32 arg0) {
-    register u8 *base asm("r1") = D_030068E8;
-    register u32 offset asm("r0") = arg0;
+    u8 *base;
+    u32 offset;
+
+    base = D_030068E8;
+    offset = arg0;
     offset <<= 5;
     offset += (u32)base;
     return ((u32)*(u8 *)offset << 0x1F) >> 0x1F;
