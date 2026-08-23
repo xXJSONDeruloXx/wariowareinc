bf8b8e50a33f9408f46e45472dabe918f62c29b1
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-08-23 11:48:24 -0400

    refactor: remove register pins from song wrappers

--- src/decomp/asm_0801915c.c
+++ src/decomp/asm_0801915c.c
@@ -5,8 +5,11 @@ extern void *play_sound(struct SongHeader *);
 extern u8 D_083AE438[];
 
 void func_0801915C(void) {
-    register u8 *base asm("r4") = D_083AE438;
-    u32 offset = (u32)get_random_range(3);
+    u8 *base;
+    u32 offset;
+
+    base = D_083AE438;
+    offset = (u32)get_random_range(3);
     offset = (offset << 0x10) >> 0x0E;
     offset += (u32)base;
     play_sound(*(struct SongHeader **)offset);
