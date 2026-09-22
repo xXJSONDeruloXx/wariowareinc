46a6fb4bc35ba10f09e8207a08edba310eaaf94f
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-09-22 17:44:29 -0400

    decomp: match sprite visibility wrappers

---
+++ src/decomp/asm_0808d2c8.c
@@ -0,0 +1,3 @@
+#include "global.h"
+#include "src/lib_sprite.h"
+void func_0808D2C8(void) { sprite_set_visible(gSpriteHandler, ((s16 *)gCurrentSceneVariable)[0x684], 1); }
---
+++ src/decomp/asm_080b2450.c
@@ -0,0 +1,3 @@
+#include "global.h"
+#include "src/lib_sprite.h"
+void func_080B2450(void) { sprite_set_visible(gSpriteHandler, ((s16 *)gCurrentSceneVariable)[0xDA], 0); }
